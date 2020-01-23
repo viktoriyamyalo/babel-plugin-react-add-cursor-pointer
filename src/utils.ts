e71a764909369b31dac1fcbd7bef5f64f46e0745
viktoriyamyalo <viktoriya.myalo.lystopad@gmail.com>
2020-01-23 13:48:54 +0200

    feat(utils): moved attribute methods to a separate AttributeHandler class.

--- src/utils.ts
+++ src/utils.ts
@@ -12,64 +12,67 @@ import {
   JSXOpeningElement,
 } from 'babel-types';
 
-function addAttribute({
-  node,
-  attribute,
-}: {
-  node: JSXOpeningElement;
-  attribute: JSXAttribute;
-}): void {
-  const { attributes } = node;
+class AttributeHandler {
+  public static addAttribute({
+    node,
+    attribute,
+  }: {
+    node: JSXOpeningElement;
+    attribute: JSXAttribute;
+  }): void {
+    const { attributes } = node;
 
-  attributes.push(attribute);
-}
+    attributes.push(attribute);
+  }
 
-function createAttribute({ name, value }: { name: JSXIdentifier; value? }): JSXAttribute {
-  return jSXAttribute(name, value);
-}
+  public static createAttribute({
+    name,
+    value,
+  }: {
+    name: JSXIdentifier;
+    value?;
+  }): JSXAttribute {
+    return jSXAttribute(name, value);
+  }
 
-function getAttribute({
-  node,
-  attributeName,
-}: {
-  node: JSXOpeningElement;
-  attributeName: string;
-}): JSXAttribute | null {
-  return node.attributes && hasAttribute({ node, attributeName })
-    ? Array.from(node.attributes).find((attribute) =>
-        isJSXIdentifier(attribute.name, { name: attributeName }),
-      )
-    : null;
-}
+  public static hasAttribute({
+    node,
+    attributeName,
+  }: {
+    node: JSXOpeningElement;
+    attributeName: string;
+  }): boolean {
+    return (
+      node.attributes
+      && node.attributes.some((attribute) => isJSXIdentifier(attribute.name, { name: attributeName }))
+    );
+  }
 
-function hasAttribute({
-  node,
-  attributeName,
-}: {
-  node: JSXOpeningElement;
-  attributeName: string;
-}): boolean {
-  return (
-    node.attributes &&
-    node.attributes.some((attribute) => isJSXIdentifier(attribute.name, { name: attributeName }))
-  );
+  public static getAttribute({
+    node,
+    attributeName,
+  }: {
+    node: JSXOpeningElement;
+    attributeName: string;
+  }): JSXAttribute | null {
+    return node.attributes && AttributeHandler.hasAttribute({ node, attributeName })
+      ? Array.from(node.attributes).find((attribute) => isJSXIdentifier(attribute.name, { name: attributeName }))
+      : null;
+  }
 }
 
 function isFunctionOrIdentifier(value): boolean {
   return (
-    isArrowFunctionExpression(value) ||
-    isClassMethod(value) ||
-    isFunctionExpression(value) ||
-    isFunctionDeclaration(value) ||
-    isObjectMethod(value) ||
-    isIdentifier(value)
+    isArrowFunctionExpression(value)
+    || isClassMethod(value)
+    || isFunctionExpression(value)
+    || isFunctionDeclaration(value)
+    || isObjectMethod(value)
+    || isIdentifier(value)
   );
 }
 
 export default {
-  addAttribute,
-  createAttribute,
-  getAttribute,
-  hasAttribute,
+  AttributeHandler,
   isFunctionOrIdentifier,
 };
